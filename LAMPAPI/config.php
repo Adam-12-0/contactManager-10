483e81209d726077f8f140a41dd86947da728ee2
ty-5 <tylertorsiello@icloud.com>
2024-05-30 13:22:28 -0400

    Update config.php

--- LAMPAPI/config.php
+++ LAMPAPI/config.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-Type: application/json');
+
 // Database connection
 $servername = "YOUR_SERVERNAME";
 $username = "YOUR_USERNAME";
@@ -10,4 +12,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
 }
+
+// Additional code to handle registration or login can be added here
+
+echo json_encode(["success" => "Connected successfully"]);
 ?>
